f7869c3cab7ebb5c0969fac87b33dba6e34283d7
Christopher Lorton <christopher.lorton@gatesfoundation.org>
2025-10-02 21:33:44 +0000

    Only print if verbose is True Must be present in parameters _and_ True.

--- src/laser_cholera/metapop/analyzer.py
+++ src/laser_cholera/metapop/analyzer.py
@@ -20,13 +20,15 @@ class Analyzer:
         if tick == model.params.nticks - 1:
             if ("calc_likelihood" in model.params) and model.params.calc_likelihood:
                 # Use the smaller of reported cases or the number of timesteps (not including the initial state)
-                nreports = min(model.params.reported_cases.shape[1], model.patches.incidence.shape[0] - 1)
+                nreports = min(
+                    model.params.reported_cases.shape[1], model.patches.incidence.shape[0] - 1)
                 try:
                     model.log_likelihood = get_model_likelihood(
                         obs_cases=model.params.reported_cases[:, :nreports],
-                        sim_cases=model.patches.incidence[1 : nreports + 1, :].T,
+                        sim_cases=model.patches.incidence[1: nreports + 1, :].T,
                         obs_deaths=model.params.reported_deaths[:, :nreports],
-                        sim_deaths=model.patches.disease_deaths[1 : nreports + 1, :].T,
+                        sim_deaths=model.patches.disease_deaths[1: nreports + 1, :].T,
+                        verbose=model.params.verbose if "verbose" in model.params else False,
                     )
                 except ValueError as e:
                     print(f"Error calculating log likelihood: {e}")
@@ -37,12 +39,14 @@ class Analyzer:
         return
 
     def plot(self, fig: Figure = None):  # pragma: no cover
-        _fig = plt.figure(figsize=(12, 9), dpi=128, num="SIRV Trajectories (Largest Patch)") if fig is None else fig
+        _fig = plt.figure(figsize=(
+            12, 9), dpi=128, num="SIRV Trajectories (Largest Patch)") if fig is None else fig
 
         for ipatch in np.argsort(self.model.params.S_j_initial)[-1:]:
             for channel in ["S", "Isym", "Iasym", "R", "V1", "V2"]:
                 # Transpose ticks (:) and location since results are transposed for R users.
-                plt.plot(getattr(self.model.results, channel)[ipatch, :], label=f"{channel}")
+                plt.plot(getattr(self.model.results, channel)
+                         [ipatch, :], label=f"{channel}")
         plt.xlabel("Tick")
         plt.ylabel("Population")
         plt.legend()
