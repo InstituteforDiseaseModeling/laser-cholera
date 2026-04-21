dba6b35f62917af3802210c772884dfdc9b3f53c
clorton <clorton@users.noreply.github.com>
2026-04-21 23:03:19 +0000

    Bump version: 0.12.4 → 0.12.5

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ project = "LASER Cholera (LASIK)"
 year = "2024-2025"
 author = "Institute for Disease Modeling"
 copyright = f"{year}, Bill & Melinda Gates Foundation"
-version = release = "0.12.4"
+version = release = "0.12.5"
 
 pygments_style = "trac"
 templates_path = ["."]
